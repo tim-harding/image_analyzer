d33e472efd621fb44bb8dd41f631c830327561a0
Tim Harding <Tim@TimHarding.co>
2020-06-06 11:45:32 -0700

    Some sort of CSV output working

--- src/bin/analyzer.rs
+++ src/bin/analyzer.rs
@@ -4,8 +4,24 @@ use clap::Clap;
 fn main() -> Result<()> {
     let opts = bright::Opts::parse();
     let files = std::fs::read_dir(opts.dir)?;
+    let mut out = csv::Writer::from_path(opts.out)?;
     for file in files {
         let file = file?;
+
+        let path = file.path();
+        let exposure = path
+            .file_name()
+            .ok_or(anyhow!("No file name"))?
+            .to_str()
+            .ok_or(anyhow!("could not convert to string"))?
+            .split('.')
+            .next()
+            .ok_or(anyhow!("No extension"))?
+            .split('-')
+            .skip(1)
+            .next()
+            .ok_or(anyhow!("No exposure part in name"))?;
+
         let img = image::open(file.path())?;
         match img {
             image::DynamicImage::ImageRgb8(img) => {
@@ -14,7 +30,7 @@ fn main() -> Result<()> {
                     average += pixel[0] as u64;
                 }
                 average /= img.width() as u64 * img.height() as u64;
-                println!("{}", average);
+                out.write_record(&[exposure, &format!("{}", average)])?;
             }
             _ => {}
         }
